3886937c39cf076356bf5cf06d54695ab40742de
Stephen Adams <stephen@cgcsoftware.co.uk>
2018-12-29 12:59:13 +0000

    Created new Search Form presentational component
    Created a new Search Form component
    Added this to the Client Page component
    Removed Angular Logo from App component

--- Client-Contacts-Manager-Angular/src/app/app.module.ts
+++ Client-Contacts-Manager-Angular/src/app/app.module.ts
@@ -5,11 +5,13 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ClientPageComponent } from './clients/client-page/client-page.component';
+import { SearchFormComponent } from './search/search-form/search-form.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ClientPageComponent
+    ClientPageComponent,
+    SearchFormComponent
   ],
   imports: [
     BrowserModule,
