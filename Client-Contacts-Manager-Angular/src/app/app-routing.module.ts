cad0ca4755ce6c3dbce474cc875ca21200a18712
Stephen Adams <stephen@cgcsoftware.co.uk>
2018-12-29 12:55:06 +0000

    Added basic routing to Clients page
    Added a basic routing to load the ClientPage smart component. Also added some basic styling to the main app component

--- Client-Contacts-Manager-Angular/src/app/app-routing.module.ts
+++ Client-Contacts-Manager-Angular/src/app/app-routing.module.ts
@@ -1,10 +1,21 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ClientPageComponent } from './clients/client-page/client-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'clients',
+    component: ClientPageComponent
+  },
+  {
+    path: '',
+    redirectTo: '/clients',
+    pathMatch: 'full'
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- Client-Contacts-Manager-Angular/src/app/app.module.ts
+++ Client-Contacts-Manager-Angular/src/app/app.module.ts
@@ -4,10 +4,12 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ClientPageComponent } from './clients/client-page/client-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ClientPageComponent
   ],
   imports: [
     BrowserModule,
