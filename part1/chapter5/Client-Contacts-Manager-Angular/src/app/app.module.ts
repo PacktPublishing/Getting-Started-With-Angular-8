9a64120f53a0387c7aeabb68d32f7bbacee2c692
Stephen Adams <stephen@cgcsoftware.co.uk>
2019-01-18 21:33:27 +0000

    Added new modules
    Added the new ClientModule and CompanyModule

--- part1/chapter5/Client-Contacts-Manager-Angular/src/app/app.module.ts
+++ part1/chapter5/Client-Contacts-Manager-Angular/src/app/app.module.ts
@@ -8,14 +8,22 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ClientPageComponent } from './clients/client-page/client-page.component';
 import { SearchFormComponent } from './search/search-form/search-form.component';
 import { ClientFormComponent } from './clients/client-form/client-form.component';
-
+import { ClientModule } from './clients/client.module';
+import { CompanyModule } from './company/company.module';
 
 @NgModule({
-  declarations: [AppComponent, ClientPageComponent, SearchFormComponent, ClientFormComponent],
+  declarations: [
+    AppComponent,
+    ClientPageComponent,
+    SearchFormComponent,
+    ClientFormComponent
+  ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
+    ClientModule,
+    CompanyModule,
     FormsModule,
     ReactiveFormsModule
   ],
